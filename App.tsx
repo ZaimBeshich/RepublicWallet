2c5de2baa2b3267a44f1e7148c64197824d6fb3c
Zaim_Beshich <zaimbeshich93@gmail.com>
2025-05-18 22:25:57 +0400

    fix: use context

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Navigation} from './src/navigation/Navigation';
 import {theme} from './src/res/theme';
+import {AppProvider, useApp} from './src/context/AppContext';
+
+const AppContent = () => {
+  const {init} = useApp();
+
+  useEffect(() => {
+    init();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <Navigation theme={theme} />;
+};
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <Navigation theme={theme} />
+      <AppProvider>
+        <AppContent />
+      </AppProvider>
     </SafeAreaProvider>
   );
 }
