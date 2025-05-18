d89c487de22ec0b98e45f6442959dd7a4bc20142
Zaim_Beshich <zaimbeshich93@gmail.com>
2025-05-18 19:34:15 +0400

    fix: move transaction item to separate component

---
+++ src/components/TransactionItem.tsx
@@ -0,0 +1,127 @@
+import React, {memo} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {ScaledSheet} from 'react-native-size-matters';
+import {colors} from '../res/colors';
+import {Transaction, TransactionStatus} from '../mock/mockDataTransactions';
+import {formatAmount} from '../utils/helpers';
+import FastImage from 'react-native-fast-image';
+import Checkmark from '../assets/checkMark.png';
+import Cross from '../assets/redCross.png';
+import Clock from '../assets/clock.png';
+
+interface TransactionItemProps {
+  item: Transaction;
+  onPress: (item: Transaction) => void;
+}
+
+interface TransactionImageProps {
+  type: TransactionStatus;
+}
+
+const TransactionImage = memo(({type}: TransactionImageProps) => {
+  let image = Checkmark;
+  switch (type) {
+    case 'completed':
+      image = Checkmark;
+      break;
+    case 'pending':
+      image = Clock;
+      break;
+    case 'failed':
+      image = Cross;
+      break;
+  }
+  return (
+    <FastImage
+      source={image}
+      resizeMode="contain"
+      style={styles.transactionImage}
+    />
+  );
+});
+
+export const TransactionItem = ({item, onPress}: TransactionItemProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={styles.transactionItem}
+      onPress={() => onPress(item)}>
+      <View style={styles.transactionBlock}>
+        <TransactionImage type={item.status} />
+        <View style={styles.transactionInfo}>
+          <Text
+            style={styles.transactionDescription}
+            numberOfLines={1}
+            ellipsizeMode="tail">
+            {item.description}
+          </Text>
+          <Text style={styles.text}>{item.status}</Text>
+        </View>
+      </View>
+      <View style={styles.amountContainer}>
+        <Text
+          style={[
+            styles.amountText,
+            item.type === 'income'
+              ? styles.amountTextPlus
+              : styles.amountTextMinus,
+          ]}>
+          {`${item.type === 'income' ? '+' : '-'} ${formatAmount({
+            amount: item.amount,
+            currency: item.currency,
+          })}`}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = ScaledSheet.create({
+  transactionItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: '8@mvs',
+  },
+  transactionBlock: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: '8@s',
+    flexShrink: 1,
+  },
+  transactionInfo: {
+    flexShrink: 1,
+    flexDirection: 'column',
+    gap: '8@vs',
+    minWidth: 0,
+  },
+  amountContainer: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    flexShrink: 0,
+  },
+  amountText: {
+    fontSize: '14@ms',
+    fontWeight: '500',
+    textAlign: 'right',
+  },
+  amountTextPlus: {
+    color: colors.system.green,
+  },
+  amountTextMinus: {
+    color: colors.system.red,
+  },
+  text: {
+    color: colors.text.primary,
+  },
+  transactionDescription: {
+    fontSize: '16@ms',
+    fontWeight: '500',
+    color: colors.text.primary,
+  },
+  transactionImage: {
+    width: '24@ms',
+    height: '24@ms',
+  },
+});
--- src/screens/TransactionDetails/TransactionDetails.tsx
+++ src/screens/TransactionDetails/TransactionDetails.tsx
@@ -5,6 +5,10 @@ import Header from '../../components/Header/Header';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {HomeStackParamList} from '../../types/navigation';
 import {SCREEN_TRANSACTION_DETAILS} from '../../res/routes';
+import {colors} from '../../res/colors';
+import moment from 'moment';
+import {formatAmount} from '../../utils/helpers';
+import {Separator} from '../../components/Separator';
 
 type Props = NativeStackScreenProps<
   HomeStackParamList,
@@ -16,9 +20,39 @@ const TransactionDetails = ({route}: Props) => {
   console.log({item});
   return (
     <View style={styles.container}>
-      <Header title="Transaction" />
+      <Header
+        title={
+          item.status === 'pending' ? 'Blocked Amount' : 'Transaction Details'
+        }
+      />
       <View style={styles.content}>
+        <Text style={styles.subtitle}>
+          {moment(item.date).format('DD MMM, YYYY')}
+        </Text>
         <Text style={styles.description}>{item.description}</Text>
+        <View style={styles.block}>
+          <Separator />
+          <Text style={styles.amount}>
+            {` ${item.type === 'income' ? '+' : '-'} ${formatAmount({
+              amount: item.amount,
+              currency: item.currency,
+            })}`}
+          </Text>
+          <Separator />
+        </View>
+        <Text style={styles.moreDetails}>{'More details:'}</Text>
+        <View style={styles.row}>
+          <Text style={styles.leftText}>{'Type:'}</Text>
+          <Text style={styles.rightText}>{item.type}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.leftText}>{'Category:'}</Text>
+          <Text style={styles.rightText}>{item.category}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.leftText}>{'Status:'}</Text>
+          <Text style={styles.rightText}>{item.status}</Text>
+        </View>
       </View>
     </View>
   );
@@ -30,11 +64,44 @@ const styles = ScaledSheet.create({
   container: {
     flex: 1,
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: '4@mvs',
+  },
   content: {
     padding: '16@ms',
   },
+  subtitle: {
+    fontSize: '16@ms',
+    color: colors.text.tertiary,
+  },
   description: {
     fontSize: '16@ms',
-    color: '#000',
+    color: colors.text.primary,
+  },
+  block: {
+    marginVertical: '10@mvs',
+  },
+  amount: {
+    fontSize: '24@ms',
+    color: colors.text.primary,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    marginVertical: '4@mvs',
+  },
+  moreDetails: {
+    fontSize: '24@ms',
+    color: colors.text.primary,
+    marginVertical: '4@mvs',
+  },
+  leftText: {
+    fontSize: '14@ms',
+    color: colors.text.tertiary,
+  },
+  rightText: {
+    fontSize: '15@ms',
+    color: colors.text.primary,
   },
 });
